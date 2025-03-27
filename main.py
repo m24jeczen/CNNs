d1fe55615ba7d7a12759fc849f1ac488774470d1
Magdalena Jeczeń <magdalena.jeczen.studia@gmail.com>
2025-03-27 18:02:00 +0100

    clean

--- main.py
+++ main.py
@@ -86,20 +86,20 @@ def run_test(batch_size = batch_size, lr=lr, epochs=epochs, l2_reg=l2_reg, augme
 # print('-------------- SUCCESS 1----------------')
 
 # One cycle test
-torch.manual_seed(42)
-scheduler_type = "one_cycle"  # "one_cycle" or "step_decay"
-augmentations=["rotation", "translation"] # rotation "translation", "noise"
-lr = 0.001
-l2_reg = 0.0
-epochs = 30
-dropout_p = 0.3
+# torch.manual_seed(42)
+# scheduler_type = "one_cycle"  # "one_cycle" or "step_decay"
+# augmentations=["rotation", "translation"] # rotation "translation", "noise"
+# lr = 0.001
+# l2_reg = 0.0
+# epochs = 30
+# dropout_p = 0.3
 
-train_loader, test_loader = get_dataloaders(batch_size=128, data_dir="./data", augmentations=augmentations)
+# train_loader, test_loader = get_dataloaders(batch_size=128, data_dir="./data", augmentations=augmentations)
 
-model_deep_cnn = DeepCNN(p_dropout=dropout_p)
-print('--- Deep CNN ---')
-train_model(model_deep_cnn, train_loader, lr=lr, epochs=epochs, l2_reg=l2_reg, augmentations=augmentations, scheduler_type=scheduler_type, dropout_p=dropout_p)
-evaluate_model(model_deep_cnn, test_loader, num_epochs=epochs, l2_reg=l2_reg, augmentations=augmentations, scheduler_type=scheduler_type, dropout_p=dropout_p)
+# model_deep_cnn = DeepCNN(p_dropout=dropout_p)
+# print('--- Deep CNN ---')
+# train_model(model_deep_cnn, train_loader, lr=lr, epochs=epochs, l2_reg=l2_reg, augmentations=augmentations, scheduler_type=scheduler_type, dropout_p=dropout_p)
+# evaluate_model(model_deep_cnn, test_loader, num_epochs=epochs, l2_reg=l2_reg, augmentations=augmentations, scheduler_type=scheduler_type, dropout_p=dropout_p)
 
 # save_model(model_deep_cnn, "Deep_CNN_one_cycle2", epoch=epochs)
 
@@ -124,7 +124,7 @@ evaluate_model(model_deep_cnn, test_loader, num_epochs=epochs, l2_reg=l2_reg, au
 
 # save_model(model_cnn_with_fc, "MLP_Mixer_one_cycle", epoch=epochs)
 
-print('-------------- SUCCESS 2----------------')
+# print('-------------- SUCCESS 2----------------')
 
 # loaded_model = load_model(DeepCNN, "Deep_CNN_test3", epoch=epochs)
 # evaluate_model(loaded_model, test_loader, num_epochs=epochs, l2_reg=0.0, augmentations=augmentations)
